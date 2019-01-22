da24dfd25c7bacfc09c34aeb1ab6007df651fd22
Jonathan Fuhr <jonathan.fuhr@student.uni-tuebingen.de>
2019-01-22 15:04:58 +0100

    ready to push

---
+++ first_script.R
@@ -0,0 +1,11 @@
+# This is the first R Script that I will include in my git repository
+
+library(tidyverse)
+
+data("diamonds")
+
+# My first branch
+
+# conflict in master
+# Try to cause conflict
+# conflict resolved.
